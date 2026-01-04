dfc82905ab50f16889484026e3b602056d7d8ab7
Magnus Markling <magnus@markling.com>
2026-01-04 14:14:35 +0100

    wip

--- boards/pimoroni-tufty2040/examples/tufty_demo.rs
+++ boards/pimoroni-tufty2040/examples/tufty_demo.rs
@@ -7,13 +7,21 @@
 #![no_std]
 #![no_main]
 
+use embedded_hal_bus::spi::ExclusiveDevice;
+use fugit::RateExtU32 as _;
+use mipidsi::{interface::SpiInterface, models::ST7789, Builder};
 use pimoroni_tufty2040 as tufty;
 
+use rp2040_hal::Spi;
+use static_cell::StaticCell;
 // The macro for our start-up function
 use tufty::entry;
 
 // GPIO traits
-use embedded_hal::digital::{OutputPin, PinState};
+use embedded_hal::{
+    digital::{OutputPin, PinState},
+    spi::MODE_0,
+};
 
 // Ensure we halt the program on panic (if we don't mention this crate it won't
 // be linked)
@@ -40,7 +48,8 @@ use embedded_graphics::pixelcolor::{Rgb565, RgbColor};
 use embedded_graphics::primitives::{Circle, Primitive, PrimitiveStyleBuilder};
 use embedded_graphics::Drawable;
 use embedded_hal::delay::DelayNs;
-use st7789::ST7789;
+
+static SPI_BUFFER: StaticCell<[u8; 1024]> = StaticCell::new();
 
 #[entry]
 fn main() -> ! {
@@ -122,9 +131,33 @@ fn main() -> ! {
         display_data,
     );
 
-    let mut display = ST7789::new(display_interface, DummyPin, 240, 320);
-    display.init(&mut delay).unwrap();
-    display.clear(Rgb565::BLUE).unwrap();
+    // let mut display = ST7789::new(display_interface, DummyPin, 240, 320);
+    // display.init(&mut delay).unwrap();
+    // display.clear(Rgb565::BLUE).unwrap();
+
+    //
+
+    let dc = pins.spi_miso.reconfigure();
+    let cs = pins.lcd_cs.reconfigure();
+    let spi_sclk = pins.spi_sclk.reconfigure();
+    let spi_mosi = pins.spi_mosi.reconfigure();
+
+    let spi_bus = Spi::new(pac.SPI0, (spi_mosi, spi_sclk)).init(
+        &mut pac.RESETS,
+        125u32.MHz(),
+        16u32.MHz(),
+        MODE_0,
+    );
+    let spi_device = ExclusiveDevice::new_no_delay(spi_bus, cs).unwrap();
+    let spi_buffer = SPI_BUFFER.init([0; _]);
+    let di = SpiInterface::new(spi_device, dc, spi_buffer);
+    let mut screen = Builder::new(ST7789, di)
+        .display_size(240, 240)
+        .init(&mut Hal10Delay(delay))
+        .unwrap();
+    screen.clear(Rgb565::BLACK).unwrap();
+
+    //
 
     let style = PrimitiveStyleBuilder::default()
         .fill_color(Rgb565::RED)
@@ -143,5 +176,30 @@ fn main() -> ! {
         // LED off, and wait for 500ms
         led_pin.set_low().unwrap();
         timer.delay_ms(500);
+    }
+}
+
+// Newtype wrapper implementing the delay trait from EH 1.0.
+struct Hal10Delay<'a>(&'a mut Delay);
+
+// This block is copied from (the currently unreleased) commit 5573370 of the cortex-m crate.
+impl<'a> DelayNs for Hal10Delay<'a> {
+    #[inline]
+    fn delay_ns(&mut self, ns: u32) {
+        // from the rp2040-hal:
+        let us = ns / 1000 + if ns % 1000 == 0 { 0 } else { 1 };
+        // With rustc 1.73, this can be replaced by:
+        // let us = ns.div_ceil(1000);
+        Delay::delay_us(self.0, us)
+    }
+
+    #[inline]
+    fn delay_us(&mut self, us: u32) {
+        Delay::delay_us(self.0, us)
+    }
+
+    #[inline]
+    fn delay_ms(&mut self, ms: u32) {
+        Delay::delay_ms(self.0, ms)
     }
 }
